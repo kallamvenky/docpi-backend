3d30a272e05a096037eb0aa15bd0069aeff1898a
kallam venkatesh <kallamvenky@gmail.com>
2023-05-26 08:00:16 +0530

    changes

--- docpi_app/api/aws.py
+++ docpi_app/api/aws.py
@@ -1,9 +1,9 @@
 import boto3
 import pandas as pd
-from docpi_app.connection import access_key_id, secret_access_key, region_name
+# from docpi_app.connection import access_key_id, secret_access_key, region_name
 def upload_file_to_s3(file_path, bucket_name, object_name):
     # Create an S3 client
-    s3 = s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, region_name= region_name)
+    s3 = s3 = boto3.client('s3')
 
     # Upload the file to S3
     s3.upload_fileobj(file_path, bucket_name, object_name)
@@ -11,7 +11,7 @@ def upload_file_to_s3(file_path, bucket_name, object_name):
 def read_csv_from_s3(bucket_name, object_name):
     expiration_time = 3600
     # Create an S3 client
-    s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, region_name= region_name)
+    s3 = boto3.client('s3')
 
     # Generate a pre-signed URL for the S3 object
     url = s3.generate_presigned_url(
